ee4430a6c9385185f0dbda90a5a402cc96728710
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-09-16 22:37:46 -0700

    Setup API calls

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "a6ea21e7-b945-4061-b772-a06d9485dee4" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
