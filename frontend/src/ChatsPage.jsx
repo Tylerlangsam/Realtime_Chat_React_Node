2c2cf9b96d3d42c7f796d4a29b39b6a8ee39e851
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-09-21 15:26:04 -0700

    Connect backend to frontend

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,16 @@
-const ChatsPage = () => {
-  return <div className="background">chats...</div>;
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "06960625-d5a3-49df-bb60-56fc31fc6684",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
 };
 export default ChatsPage;
